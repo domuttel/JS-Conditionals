7a8d830dbba519eedc2aa542f1ce2ab5e0a32f73
Dominic Muttel <dominic.muttel@gmail.com>
2015-07-13 15:59:03 -0600

    First commit for exercise

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-// var num = prompt("Please enter your phone number including dashes");
-//
-// if (num.charAt(3) && num.charAt(7) === '-'){
-//   alert("Thank you for you phone number\!");
-// } else {
-//   alert("Invalid phone number\!")
-// }
+var num = prompt("Please enter your phone number including dashes");
+
+if (num.charAt(3) && num.charAt(7) === '-'){
+  alert("Thank you for you phone number\!");
+} else {
+  alert("Invalid phone number\!")
+}
 
 var zip = prompt("Please enter your postal code. ex. 12345 or 12345-1234");
 
-if (zip.charAt(5) === '-'){
+if (zip.length === 5){
   alert("Thank you for your zip code\!");
-} else if (zip.charAt(5) === ' '){
+} else if (zip.length === 10 && zip.charAt(5) === '-'){
   alert("Thank you for your zip code\!");
 } else {
   alert("Invalid zip code\!")
 }
+
+var state = prompt("Please enter your two letter state abriviation in uppercase.")
+
+if (state.length === 2){
+  alert("Thank you for providing your state abbreviation\!");
+} else {
+  alert("Invalid state abbreviation\!")
+}
+
+  // - married ("yes" or "no" or "YES" or "NO" or "Yes" or "No")
+
+var married = prompt("Are you married? yes or no")
+
+  if (married.length === 3)
