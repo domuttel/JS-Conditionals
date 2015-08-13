559301e5dbe121290c8ae7fb179be8cab6e46788
Dominic Muttel <dominic.muttel@gmail.com>
2015-08-13 17:17:36 -0600

    edits and finished marriage question

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
 var num = prompt("Please enter your phone number including dashes");
 
 if (num.charAt(3) && num.charAt(7) === '-'){
-  alert("Thank you for you phone number\!");
+  alert("Thank you for you phone number!");
 } else {
-  alert("Invalid phone number\!")
+  alert("Invalid phone number!");
 }
 
 var zip = prompt("Please enter your postal code. ex. 12345 or 12345-1234");
 
 if (zip.length === 5){
-  alert("Thank you for your zip code\!");
+  alert("Thank you for your zip code!");
 } else if (zip.length === 10 && zip.charAt(5) === '-'){
-  alert("Thank you for your zip code\!");
+  alert("Thank you for your zip code!");
 } else {
-  alert("Invalid zip code\!")
+  alert("Invalid zip code!");
 }
 
 var state = prompt("Please enter your two letter state abriviation in uppercase.")
 
 if (state.length === 2){
-  alert("Thank you for providing your state abbreviation\!");
+  alert("Thank you for providing your state abbreviation!");
 } else {
-  alert("Invalid state abbreviation\!")
+  alert("Invalid state abbreviation!");
 }
 
-  // - married ("yes" or "no" or "YES" or "NO" or "Yes" or "No")
-
-var married = prompt("Are you married? yes or no")
-
-  if (married.length === 3)
+var married = prompt("Are you married? yes or no");
+   var makeLow = married.toLowerCase();
+  if (makeLow === "yes" || makeLow === "no") {
+    alert("Thank you for your maritial status");
+  } else {
+    alert("Invalid entry!");
+  }
